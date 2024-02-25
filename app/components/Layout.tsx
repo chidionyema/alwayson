6ff18f5f9e76920893c54cb1e65da594a743d13e
chidionyema <chidio@risual.com>
2024-02-25 16:29:08 +0000

    Done

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,4 +1,3 @@
-// components/Layout.tsx
 import React, { ReactNode } from 'react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
@@ -11,10 +10,13 @@ type Props = {
 const Layout: React.FC<Props> = ({ children, toggleTheme, theme }) => {
   return (
     <main className={`flex flex-col items-center p-8 md:p-16 lg:p-24 xl:p-32 ${theme === 'dark' ? 'bg-gray-800' : 'bg-gray-100'} min-h-screen transition duration-500 ease-in-out`}>
-      <header className="w-full flex justify-between items-center mb-12">
-        <h1 className="text-2xl font-bold ${theme === 'dark' ? 'text-gray-100' : 'text-gray-900'}">Always On Technologies</h1>
+      <header className="w-full flex justify-between items-center mb-12 text-center">
+        <div className="flex-1"></div> {/* This div is used to balance the header content */}
+        <h1 className={`flex-1 text-2xl font-bold ${theme === 'dark' ? 'text-gray-100' : 'text-gray-900'}`}>
+          Always On Technologies
+        </h1>
         {toggleTheme && (
-          <button onClick={toggleTheme} className="text-gray-600 dark:text-gray-300">
+          <button onClick={toggleTheme} className="flex-1 text-right text-gray-600 dark:text-gray-300">
             {theme === 'dark' ? <FaSun className="text-xl" /> : <FaMoon className="text-xl" />}
           </button>
         )}
