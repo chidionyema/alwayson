228f0221e4d25f61253bbfd3baae7612dc57bd08
chidionyema <chidio@risual.com>
2024-02-25 23:44:57 +0000

    done

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,11 +7,12 @@ const ContactPage = () => {
   return (
     <Layout>
       <div className="page-container">
+      <h1 style={{ color: '#0f2b46', fontWeight: 'bold' }} >Contact Us</h1>
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <div>
             {/* Heading consistent with the Consultant Profile Page */}
-            <h1 style={{ color: '#0f2b46', fontWeight: 'bold' }} >Contact Us</h1>
-
+           
             <p className="text-lg mb-6">
   {"We're eager to hear from you. Use the form below to send us a message, ask a question, or request a consultation. Our team will get back to you as soon as possible."}
 </p>
