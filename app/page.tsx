474fcefed50aa4c8c107da29f6cfc8a512e6fc9c
chidionyema <chidio@risual.com>
2024-02-27 03:18:39 +0000

    done

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ const HomePage: React.FC = () => {
   const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
 
   return (
-    <Layout>
+
       <div className="py-10 lg:py-16 px-4 max-w-screen-xl mx-auto">
         <h2 className={`text-4xl lg:text-5xl font-bold mb-6 ${theme === 'dark' ? 'text-gray-100' : 'text-gray-900'}`}>
           Empowering Your Digital Future
@@ -72,7 +72,7 @@ const HomePage: React.FC = () => {
           ))}
         </div>
       </div>
-    </Layout>
+ 
   );
 };
 
