f21282743f862c238af08935ec14d44742d961af
chidionyema <chidio@risual.com>
2024-02-27 01:08:19 +0000

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from 'react';
 import { FaLaptopCode, FaUserCog, FaCloud, FaServer, FaMobileAlt, FaRobot } from 'react-icons/fa';
 import Layout from './components/Layout';
