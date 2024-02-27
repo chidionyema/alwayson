f7b2c6d52412b4ede588bec4cf445eca644ad5f5
chidionyema <chidio@risual.com>
2024-02-27 00:50:06 +0000

    done

--- app/_app.tsx
+++ app/_app.tsx
@@ -5,13 +5,11 @@
 import 'global.css';
 import React from 'react';
 import { AppProps } from 'next/app';
-import { ThemeProvider } from './ThemeContext';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider>
       <Component {...pageProps} />
-    </ThemeProvider>
+
   );
 };
 
