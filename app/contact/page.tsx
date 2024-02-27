552f15b1a16f89c2fa7b28e4fe8dbe30adc09d33
chidionyema <chidio@risual.com>
2024-02-27 01:16:50 +0000

    done

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,9 +9,10 @@ const ContactPage = () => {
         <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-[#0f2b46] mb-6">Contact Us</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <div>
-            <p className="text-lg mb-6">
-              We're eager to hear from you. Use the form below to send us a message, ask a question, or request a consultation. Our team will get back to you as soon as possible.
-            </p>
+          <p className="text-lg mb-6">
+  We&rsquo;re eager to hear from you. Use the form below to send us a message, ask a question, or request a consultation. Our team will get back to you as soon as possible.
+</p>
+
           </div>
           <form className="w-full max-w-lg mx-auto md:mx-0">
             {/* Form fields */}
